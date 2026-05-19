c8f2c88cb5da245c2196d24b5e677dcd2983b5c7
FRANK AMADI <frankitchy@FRANKs-MacBook-Pro.local>
2026-05-19 16:57:06 +0100

    Fix school data format and STATE_TO_ZONE mapping

--- assets/js/schools-data.js
+++ assets/js/schools-data.js
@@ -1,52 +1,52 @@
 const SCHOOL_DATA = {
     northEast: [
-        "Federal College of Education (Special)", "Jalingo",
-        "Federal Government Day Secondary School", "Jalingo",
-        "Federal Government Girls' Secondary School", "Jalingo",
-        "Federal Government Boys' Secondary School", "Jalingo",
-        "Federal Science and Technical College", "Jalingo",
-        "Adamu Atta Memorial Secondary School", "Jalingo"
+        "Federal College of Education (Special) - Jalingo",
+        "Federal Government Day Secondary School - Jalingo",
+        "Federal Government Girls' Secondary School - Jalingo",
+        "Federal Government Boys' Secondary School - Jalingo",
+        "Federal Science and Technical College - Jalingo",
+        "Adamu Atta Memorial Secondary School - Jalingo"
     ],
     northCentral: [
-        "Federal College of Education (Technical)", "Minna",
-        "Federal Government Day Secondary School", "Minna",
-        "Federal Government Girls' Secondary School", "Minna",
-        "Federal Government Boys' Secondary School", "Minna",
-        "Federal Science and Technical College", "Minna",
-        "Ahmadu Bello University", "Zaria",
-        "Federal University", "Nigeria (Main Campus)", "Keffi"
+        "Federal College of Education (Technical) - Minna",
+        "Federal Government Day Secondary School - Minna",
+        "Federal Government Girls' Secondary School - Minna",
+        "Federal Government Boys' Secondary School - Minna",
+        "Federal Science and Technical College - Minna",
+        "Ahmadu Bello University - Zaria",
+        "Federal University, Nigeria (Main Campus) - Keffi"
     ],
     northWest: [
-        "Federal College of Education (Special)", "Kano",
-        "Federal Government Day Secondary School", "Kano",
-        "Federal Government Girls' Secondary School", "Kano",
-        "Federal Government Boys' Secondary School", "Kano",
-        "Federal Science and Technical College", "Kano",
-        "Kano State University", "Kano"
+        "Federal College of Education (Special) - Kano",
+        "Federal Government Day Secondary School - Kano",
+        "Federal Government Girls' Secondary School - Kano",
+        "Federal Government Boys' Secondary School - Kano",
+        "Federal Science and Technical College - Kano",
+        "Kano State University - Kano"
     ],
     southEast: [
-        "Federal College of Education (Technical)", "Omuma",
-        "Federal Government Day Secondary School", "Omuma",
-        "Federal Government Girls' Secondary School", "Omuta",
-        "Federal Government Boys' Secondary School", "Omuta",
-        "Federal Science and Technical College", "Omuta",
-        "University of Nigeria", "Nsukka"
+        "Federal College of Education (Technical) - Omuma",
+        "Federal Government Day Secondary School - Omuma",
+        "Federal Government Girls' Secondary School - Omuta",
+        "Federal Government Boys' Secondary School - Omuta",
+        "Federal Science and Technical College - Omuta",
+        "University of Nigeria - Nsukka"
     ],
     southWest: [
-        "Federal College of Education (Special)", "Lagos",
-        "Federal Government Day Secondary School", "Lagos",
-        "Federal Government Girls' Secondary School", "Lagos",
-        "Federal Government Boys' Secondary School", "Lagos",
-        "Federal Science and Technical College", "Lagos",
-        "University of Lagos", "Lagos"
+        "Federal College of Education (Special) - Lagos",
+        "Federal Government Day Secondary School - Lagos",
+        "Federal Government Girls' Secondary School - Lagos",
+        "Federal Government Boys' Secondary School - Lagos",
+        "Federal Science and Technical College - Lagos",
+        "University of Lagos - Lagos"
     ],
     southSouth: [
-        "Federal College of Education (Technical)", "Port Harcourt",
-        "Federal Government Day Secondary School", "Port Harcourt",
-        "Federal Government Girls' Secondary School", "Port Harcourt",
-        "Federal Government Boys' Secondary School", "Port Harcourt",
-        "Federal Science and Technical College", "Port Harcourt",
-        "University of Port Harcourt", "Port Harcourt"
+        "Federal College of Education (Technical) - Port Harcourt",
+        "Federal Government Day Secondary School - Port Harcourt",
+        "Federal Government Girls' Secondary School - Port Harcourt",
+        "Federal Government Boys' Secondary School - Port Harcourt",
+        "Federal Science and Technical College - Port Harcourt",
+        "University of Port Harcourt - Port Harcourt"
     ]
 };
 
@@ -73,7 +73,6 @@ const STATE_TO_ZONE = {
     "Katsina": "northWest",
     "Kebbi": "northWest",
     "Lagos": "southWest",
-    "Lauta": "northEast",
     "Nasarawa": "northCentral",
     "Niger": "northCentral",
     "Ogun": "southWest",
